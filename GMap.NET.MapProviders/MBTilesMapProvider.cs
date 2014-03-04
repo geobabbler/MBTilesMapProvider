f03b03a94adaeb2e89c059a09c5e72cbc3ba1848
Bill Dollins <bill@geomusings.com>
2014-03-04 12:25:33 -0500

    initial commit

---
+++ GMap.NET.MapProviders/MBTilesMapProvider.cs
@@ -0,0 +1,154 @@
+﻿/*
+The MIT License (MIT)
+
+Copyright (c) 2014 William Dollins (bill@geomusings.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
+
+namespace GMap.NET.MapProviders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SQLite;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Text;
+    using GMap.NET.Projections;
+    using MapTiler.GlobalMapTiles;
+    using MBTilesSupport.Helpers;
+
+    public abstract class MBTilesMapProviderBase : GMapProvider
+    {
+
+        #region GMapProvider Members
+        public override Guid Id
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override PureProjection Projection
+        {
+            get { return GMap.NET.Projections.MercatorProjection.Instance; }
+        }
+
+        GMapProvider[] overlays;
+        public override GMapProvider[] Overlays
+        {
+            get
+            {
+                if (overlays == null)
+                {
+                    overlays = new GMapProvider[] { this };
+                }
+                return overlays;
+            }
+        }
+
+        public override PureImage GetTileImage(GPoint pos, int zoom)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+    
+    public class MBTilesMapProvider : MBTilesMapProviderBase
+    {
+        internal static PureImageProxy ImageProxy;
+
+        private MBTilesHelper _mbtiles = null;
+
+
+        public MBTilesMapProvider(string dbpath)
+        {
+            _mbtiles = new MBTilesHelper(dbpath);
+            MaxZoom = _mbtiles.MaxZoom;
+            MinZoom = _mbtiles.MinZoom;
+            InvertedAxisY = true;
+            Area = new RectLatLng(_mbtiles.Bounds.West, _mbtiles.Bounds.North, _mbtiles.Bounds.East, _mbtiles.Bounds.South);
+        }
+
+        public PointLatLng Center
+        {
+            get
+            {
+                return new PointLatLng(_mbtiles.Center.Y, _mbtiles.Center.X);
+            }
+        }
+
+        #region GMapProvider Members
+
+        readonly Guid id = new Guid("EDA4CAAB-0505-4D82-9E64-72F13A66170A");
+        public override Guid Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return _mbtiles.Name;
+            }
+        }
+
+        public override PureImage GetTileImage(GPoint pos, int zoom)
+        {
+            //string url = MakeTileImageUrl(pos, zoom, LanguageStr);
+
+            return getImage(pos, zoom);
+        }
+
+        private PureImage getImage(GPoint pos, int zoom)
+        {
+            PureImage retval = null ;
+
+            var resultImage = _mbtiles.GetTileStream(pos.X, pos.Y, zoom);
+            
+                if (resultImage.Length > 0)
+                {
+                    //resultImage.Position = 0L;
+                    retval = ImageProxy.FromArray(resultImage);
+                }
+                //var stream = new MemoryStream();
+                //resultImage.Save(stream, ImageFormat.Png);
+                //stream.Position = 0L;
+                //retval.Data = stream;
+            
+            //var img = _mbtiles.GetTile(pos.X, pos.Y, zoom);
+            //img.Position = 0L;
+            
+            //retval.Data = img;
+
+            return retval;
+        }
+        #endregion
+    }
+
+}
---
+++ Helpers/MBTilesHelper.cs
@@ -0,0 +1,224 @@
+﻿/*
+The MIT License (MIT)
+
+Copyright (c) 2014 William Dollins (bill@geomusings.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using MapTiler.GlobalMapTiles;
+
+namespace MBTilesSupport.Helpers
+{
+    public class MBTilesHelper
+    {
+        public GeoExtent Bounds { get; private set; }
+        public CoordinatePair Center { get; private set; }
+        public int MinZoom { get; private set; }
+        public int MaxZoom { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string MBTilesVersion { get; private set; }
+        public string Path { get; private set; }
+
+        private GlobalMercator gmt = new GlobalMercator();
+
+        public MBTilesHelper(string path)
+        {
+            this.Path = path;
+            loadMetadata();
+        }
+
+        private void loadMetadata()
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(String.Format("Data Source={0};Version=3;", this.Path)))
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand() { Connection = conn, CommandText = "SELECT * FROM metadata;" })
+                    {
+                        SQLiteDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            string name = reader["name"].ToString();
+                            switch (name.ToLower())
+                            {
+                                case "bounds":
+                                    string val = reader["value"].ToString();
+                                    string[] vals = val.Split(new char[] { ',' });
+                                    this.Bounds = new GeoExtent() { West = Convert.ToDouble(vals[0]), South = Convert.ToDouble(vals[1]), East = Convert.ToDouble(vals[2]), North = Convert.ToDouble(vals[3]) };
+                                    break;
+                                case "center":
+                                    val = reader["value"].ToString();
+                                    vals = val.Split(new char[] { ',' });
+                                    this.Center = new CoordinatePair() { X = Convert.ToDouble(vals[0]), Y = Convert.ToDouble(vals[1]) };
+                                    break;
+                                case "minzoom":
+                                    this.MinZoom = Convert.ToInt32(reader["value"]);
+                                    break;
+                                case "maxzoom":
+                                    this.MaxZoom = Convert.ToInt32(reader["value"]);
+                                    break;
+                                case "name":
+                                    this.Name = reader["value"].ToString();
+                                    break;
+                                case "description":
+                                    this.Description = reader["value"].ToString();
+                                    break;
+                                case "version":
+                                    this.MBTilesVersion = reader["value"].ToString();
+                                    break;
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public Image GetTileLatLng(double latitude, double longtiude, int zoom)
+        {
+            Image retval = null;
+            var llt = gmt.LatLonToTile(latitude, longtiude, zoom);
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(String.Format("Data Source={0};Version=3;", Path)))
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand() { Connection = conn, CommandText = String.Format("SELECT * FROM tiles WHERE tile_column = {0} and tile_row = {1} and zoom_level = {2};", llt.X, llt.Y, zoom) })
+                    {
+                        SQLiteDataReader reader = cmd.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            byte[] bytes = reader["tile_data"] as byte[];
+                            using (MemoryStream stream = new MemoryStream(bytes))
+                            {
+                                retval = Image.FromStream(stream);
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                retval = null;
+            }
+            return retval;
+        }
+
+        public byte[] GetTileStreamLatLng(double latitude, double longtiude, int zoom)
+        {
+            byte[] retval = null;
+            var llt = gmt.LatLonToTile(latitude, longtiude, zoom);
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(String.Format("Data Source={0};Version=3;", Path)))
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand() { Connection = conn, CommandText = String.Format("SELECT * FROM tiles WHERE tile_column = {0} and tile_row = {1} and zoom_level = {2};", llt.X, llt.Y, zoom) })
+                    {
+                        SQLiteDataReader reader = cmd.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            byte[] bytes = reader["tile_data"] as byte[];
+                            retval = bytes;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                retval = null;
+            }
+            return retval;
+        }
+
+        public Image GetTile(long x, long y, int zoom)
+        {
+            Image retval = null;
+            //var llt = gmt.LatLonToTile(latitude, longtiude, zoom);
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(String.Format("Data Source={0};Version=3;", Path)))
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand() { Connection = conn, CommandText = String.Format("SELECT * FROM tiles WHERE tile_column = {0} and tile_row = {1} and zoom_level = {2};", x, y, zoom) })
+                    {
+                        SQLiteDataReader reader = cmd.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            byte[] bytes = reader["tile_data"] as byte[];
+                            using (MemoryStream stream = new MemoryStream(bytes))
+                            {
+                                retval = Image.FromStream(stream);
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                retval = null;
+            }
+            return retval;
+        }
+
+        public byte[] GetTileStream(long x, long y, int zoom)
+        {
+            byte[] retval = null;
+            //var llt = gmt.LatLonToTile(latitude, longtiude, zoom);
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(String.Format("Data Source={0};Version=3;", Path)))
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand() { Connection = conn, CommandText = String.Format("SELECT * FROM tiles WHERE tile_column = {0} and tile_row = {1} and zoom_level = {2};", x, y, zoom) })
+                    {
+                        SQLiteDataReader reader = cmd.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            byte[] bytes = reader["tile_data"] as byte[];
+                            retval = bytes; // new MemoryStream(bytes);
+                            //using (MemoryStream stream = new MemoryStream(bytes))
+                            //{
+                            //    retval = stream;
+                            //}
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                retval = null;
+            }
+            return retval;
+        }
+    }
+}
